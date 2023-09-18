34762427ba6b4daba522eab041be8009dc529950
chaseothan <52476148+chaseothan@users.noreply.github.com>
2023-09-18 15:12:30 -0400

    classwork
    classwork
    sd

--- MultipleIntentsListViewAdapter/app/src/main/java/com/example/multipleintentslistviewadapter/MainActivity.java
+++ MultipleIntentsListViewAdapter/app/src/main/java/com/example/multipleintentslistviewadapter/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -18,6 +21,10 @@ public class MainActivity extends AppCompatActivity
     Intent int_j_registerIntent;
     //Looking at class variables and what happens when we return to this class from register.java
     static int x = 5;
+    static String[] registeredUsers = new String[3];
+    static int cnt = 2;
+    ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,6 +38,28 @@ public class MainActivity extends AppCompatActivity
         //The intent you came from and the intent you want to load.
         int_j_registerIntent = new Intent(MainActivity.this, Register.class);
 
+
+        //try and see if there is a bundle passed tomaijn acc
+        //if there is parse the data
+        //if not skip
+
+        //catch info
+        Intent cameFrom = getIntent();
+        Bundle infoPassedToMe = cameFrom.getExtras();
+        //make sure we only do this is activity is loading from register.java
+        //first time this loads we dont want to do this
+        if(infoPassedToMe != null) {
+
+        //get information in the bundle
+            String fullname = infoPassedToMe.getString("Name");
+            Log.d("Register Name:", fullname + "");
+            //
+            registeredUsers[cnt] = fullname;
+            cnt++;
+            displayRegisteredUsers();
+            fillListView();
+
+        }
         registerButtonEventHandler();
     }
 
@@ -46,7 +75,26 @@ public class MainActivity extends AppCompatActivity
                 int_j_registerIntent.putExtra("InfoPassed","Hello from Main");
                 //Go to the registration intent.
                 startActivity(int_j_registerIntent);
+
             }
         });
     }
+    public void displayRegisteredUsers() {
+        for (int i = 0; i < registeredUsers.length; i++) ;
+        {
+
+
+        }
+    }
+    public void fillListView()
+    {
+            //built in list views and adapters only work with string arrays
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,registeredUsers);
+        //set list views adapter
+        lv_j_users.setAdapter(adapter);
+
+    }
+
+    }
+
 }
--- MultipleIntentsListViewAdapter/app/src/main/java/com/example/multipleintentslistviewadapter/Register.java
+++ MultipleIntentsListViewAdapter/app/src/main/java/com/example/multipleintentslistviewadapter/Register.java
@@ -7,17 +7,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.net.FileNameMap;
 
 public class Register extends AppCompatActivity
 {
     Button btn_v_r_back;
     Intent int_j_welcomeIntent;
+    Button btn_v_r_register;
+    EditText et_j_r_Fname;
+    EditText et_j_r_Lname;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         btn_v_r_back = findViewById(R.id.btn_v_r_back);
+        btn_v_r_register = findViewById(R.id.btn_v_r_register);
+        et_j_r_Fname = findViewById(R.id.et_v_r_fName);
+        et_j_r_Lname =findViewById(R.id.et_v_r_lName);
         int_j_welcomeIntent = new Intent(Register.this, MainActivity.class);
 
         //code to get info pass from mainactivity.java
@@ -30,7 +41,7 @@ public class Register extends AppCompatActivity
         //display message passed from MainActivity.java
         Log.d("Info passed from main: ", info);
 
-
+        registerButtonEventHandler();
         backButtonEventHandler();
     }
 
@@ -43,4 +54,24 @@ public class Register extends AppCompatActivity
             }
         });
     }
+    public void registerButtonEventHandler(){
+        btn_v_r_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fullname;
+
+                String fname = et_j_r_Fname.getText().toString();
+                String Lname = et_j_r_Lname.getText().toString();
+
+                fullname = fname + " " + Lname;
+
+                int_j_welcomeIntent.putExtra("Name",fullname
+                );
+                //load Intent
+                startActivity(int_j_welcomeIntent);
+            }
+        });
+    }
+
+
 }
